41758000085bd42653e56af9515595ee4451a3c9
tadghh <thenry.he@gmail.com>
2024-11-27 18:51:04 -0600

    fix: mouse in comment detection

--- wordWorker.js
+++ wordWorker.js
@@ -224,50 +224,33 @@ export class TextHighlighter {
       let hoverItem = document.getElementById(`floating-${key}`);
 
       if (hoverItem) {
+        // console.log("gover2")
         const ids = hoverItem.id
-          .replace("floating-highlighted-", "")
+          .replace("floating-", "")
           .split("-");
         const xIndex = parseInt(ids[0])
         const yIndex = parseInt(ids[1])
 
-        const yColIndex = this.findStartIndexFromIndex(xIndex);
-        const xCol = this.findXValueFromIndex(
-          yColIndex,
-
-          xIndex
-        );
+        const xCol = this.getNextLowestDivisibleByNinePointSix(this.getPaddingForIndex(xIndex))
 
         const top = this.findYValueFromIndex(yIndex);
-        const highLightedWord = this.contentTextCleaned.substring(xIndex, yIndex + 1)
         const topBorder = top;
-        const minXBorder = xCol;
         const bottomBorder = top + 20;
-        const maxXBorder = xCol + this.getWordWidth(highLightedWord)
+
+
+        const minXBorder = xCol;
+        const maxXBorder = xCol + this.getWordWidth(div.textContent)
         const newRelY = event.clientY
+
+
+
         const isInsideX = relativeX >= minXBorder && relativeX <= maxXBorder;
         const isInsideY = newRelY >= topBorder && newRelY <= bottomBorder;
         const isInside = isInsideX && isInsideY;
 
         if (isInside) {
-          const uniqueId = `hover-comment-${xIndex}-${yIndex}`;
-
-          if (!this.floatingDivsMapTwo.has(uniqueId)) {
-            const hoverComment = document.createElement("div");
-            hoverComment.id = uniqueId
-            hoverComment.textContent = "test comment here"
-            hoverComment.className = "floatingControlsTwo"
-            hoverComment.style.position = "absolute"
-            hoverComment.style.fontSize = "20px"
-            hoverComment.style.color = "white"
-            hoverComment.style.background = "green"
-            hoverComment.style.zIndex = 10
-
-            document.body.appendChild(hoverComment);
-            this.floatingDivsMapTwo.set(uniqueId, hoverComment);
-          }
-          const hoverComment = this.floatingDivsMapTwo.get(uniqueId);
-          this.positionFloatingCommentContent(hoverComment, yIndex, xIndex);
-
+          //
+          console.log("inside")
         }
       }
     });
@@ -400,8 +383,8 @@ export class TextHighlighter {
       floatingDiv.setAttribute("rawId", rawUniqueId)
 
       floatingDivContent.className = "floatingContent";
-      floatingDivContent.id = uniqueId;
-      floatingDivContent.style.width = `${contentCommentWidth}px`;
+      floatingDivContent.id = `floating-${this.startLetterIndex}-${this.endLetterIndex}`;
+      floatingDivContent.style.width = `${this.getWordWidth(demo_test)}px`;
       floatingDivContent.setAttribute("start", this.startLetterIndex)
       floatingDivContent.setAttribute("end", this.endLetterIndex)
       floatingDivContent.setAttribute("rawId", rawUniqueId)
@@ -661,50 +644,5 @@ export class TextHighlighter {
     return (this.findColFromIndex(endIndex) - this.findColFromIndex(startIndex)) + 1
   }
 
-  // TODO this helper methods in this depend on the classes content, cant use this 'cleanly'
-  createTextHighlight(startIndex, endIndex, textContent) {
-    if (startIndex > endIndex) {
-      [startIndex, endIndex] = [endIndex, startIndex];
-      startIndex++
-    }
-
-    if (this.contentTextCleaned[startIndex] === " ") startIndex++;
-    if (this.contentTextCleaned[endIndex] === " ") endIndex--;
-    // add example spans below
-    const uniqueId = `floating-highlighted-${startIndex}-${endIndex}`;
-    const rawUniqueId = `${startIndex}-${endIndex}`;
-    const selectedText = textContent.slice(startIndex, endIndex + 1);
-
-    if (!this.floatingDivsMap.has(rawUniqueId)) {
-      const floatingDiv = document.createElement("div");
-      const floatingDivContent = document.createElement("div");
-      floatingDiv.id = uniqueId;
-      floatingDivContent.id = uniqueId;
-      floatingDiv.className = "floatingControls";
-      floatingDivContent.className = "floatingContent";
-
-      let width = this.getNextLowestDivisibleByNinePointSix(this.getWordWidth(selectedText))
-
-      floatingDiv.style.width = `${width}px`;
-      floatingDiv.setAttribute("start", startIndex)
-      floatingDiv.setAttribute("end", endIndex)
-      floatingDiv.setAttribute("rawId", rawUniqueId)
-      floatingDivContent.style.width = `${width}px`;
-
-      floatingDivContent.setAttribute("start", startIndex)
-      floatingDivContent.setAttribute("end", endIndex)
-      floatingDivContent.setAttribute("rawId", rawUniqueId)
-
-      this.floatingDivsMap.set(rawUniqueId, floatingDiv);
-      this.floatingDivsMapTwo.set(rawUniqueId, floatingDivContent);
-      document.body.appendChild(floatingDiv);
-      document.body.appendChild(floatingDivContent);
-    }
-    // Add the div element relative to the span
-    this.#positionFloatingComment(this.floatingDivsMap.get(rawUniqueId));
-    this.#positionFloatingCommentContent(this.floatingDivsMapTwo.get(rawUniqueId));
-    // Initially position the div
-    this.#repositionItems()
-  }
 
 }
